109f9275ff717d5b04a59f753d4b16cd780ec211
Michael Mazilu <michaelmazilu@eduroam-campus-10-36-166-183.campus-dynamic.uwaterloo.ca>
2025-01-25 18:11:18 -0500

    first version sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
-import Image from "next/image";
+"use client";
+
+import { Textarea } from "@/components/ui/textarea";
+import { useState } from "react";
 
 export default function Home() {
-    return (
-        <div className="flex flex-col justify-center items-center h-screen">
-            <div className="text-2xl font-bold">Enter your text here</div>
-            <textarea className="w-full h-40 p-2 border border-gray-300 rounded-md text-black "></textarea>
-        </div>
-    );
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <div className="bg-[#1e1e1e] w-full flex flex-col justify-center items-center min-h-screen px-24 p">
+      <div className="text-3xl font-semibold text-pink-300 pb-16">
+        What would you like to learn?
+      </div>
+      <Textarea
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Type here..."
+        className="h-14 text-lg bg-[#2a1a29] text-white border-2 border-pink-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-pink-500 p-3 scrollable"
+      />
+    </div>
+  );
 }
---
+++ components/Sidebar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Sidebar = () => {
+  return (
+    <div className="w-64 h-screen bg-[#262626] text-white flex flex-col items-center p-4 shadow-md">
+      <h1 className="text-xl font-bold mt-4">Net Lessons</h1>
+    </div>
+  );
+};
